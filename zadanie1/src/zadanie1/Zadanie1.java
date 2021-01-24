d7162cd7a6750d879b1a20e873b8b54a3e047aec
AndroidStudio <AndroidStudio@DESKTOP-TR2IBVR>
2021-01-24 14:35:01 +0100

    Algorytm obliczający ilość słów

--- zadanie1/src/zadanie1/Zadanie1.java
+++ zadanie1/src/zadanie1/Zadanie1.java
@@ -1,11 +1,40 @@
 package zadanie1;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Zadanie1 {
 
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) throws FileNotFoundException {
 
+        File plik = new File("text.txt");
+
+//        boolean exist = plik.exists();
+//        System.out.println(exist);
+//        String abPath = plik.getAbsolutePath();
+//        System.out.println(abPath);
+        Scanner sc = new Scanner(plik);
+
+        String text = "";
+        while (sc.hasNext()) {
+            text = text + sc.nextLine() + " ";
+        }
+        System.out.println(text);
+
+        System.out.println(ileSlow(text));
     }
+
+    public static int ileSlow(String tekst) {
+        int ile_slow = 0;
+        char znak_z_napisu;
+        for (int i = 0; i < tekst.length(); i++) {
+            znak_z_napisu = tekst.charAt(i);
+            if (znak_z_napisu == ' ') {
+                ile_slow++;
+            }
+        }
+        return ile_slow;
+    }
+
 }
